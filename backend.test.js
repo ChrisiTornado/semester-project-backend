8850c0b2d94d31b8ba421e5428caee315ec8b83e
Christophe Andunda <christophe.andunda@gmail.com>
2024-06-15 23:46:28 +0000

    added jest

---
+++ backend.test.js
@@ -0,0 +1,42 @@
+
+describe('Always true tests', () => {
+    test("Test adding todo", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test connection", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if input is visible", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if input is clickable", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if input is right", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if input gets added", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if added input gets added right", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test if input is empty after pressing enter", () => {
+        expect(true).toBe(true);
+    })
+    
+    test("Test adding todo", () => {
+        expect(true).toBe(true);
+    })
+
+    test("Test checking todo", () => {
+        expect(true).toBe(true);
+    })
+  });
